234f13ba98b94b49208ecbd00eb2e92c4c359874
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2021-01-25 11:53:18 -0800

    Initial Commit

---
+++ interview/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  interview
+//
+//  Created by leo on 1/25/21.
+//  Copyright © 2021 leok7v.github.io. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
